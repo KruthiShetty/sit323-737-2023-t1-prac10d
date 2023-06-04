84a8ec1576bd46a98b61caadc7f50da508bf9754
KruthiShetty <s222067974@deakin.edu.au>
2023-06-04 19:59:14 +1000

    commit30

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My Final Changes');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST, () => {
